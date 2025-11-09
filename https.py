37aa8429fb84d2030eac57b6cd6b0dcca17daeed
MaromSv <maroms.private@gmail.com>
2025-11-09 16:02:59 +0100

    fixes

--- https.py
+++ https.py
@@ -1,4 +1,4 @@
 from pyngrok import ngrok
 
-public_url = ngrok.connect(8000)  # or your port number
+public_url = ngrok.connect("http://199.247.26.114:8000")  # or your port number
 print(public_url)
