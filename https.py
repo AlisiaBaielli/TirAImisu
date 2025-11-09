e04a38632617cfa2630d46a6122633adbe2eb121
MaromSv <maroms.private@gmail.com>
2025-11-09 16:08:27 +0100

    lets pray

--- https.py
+++ https.py
@@ -1,4 +1,8 @@
 from pyngrok import ngrok
 
-public_url = ngrok.connect("http://199.247.26.114:8000")  # or your port number
-print(public_url)
+# Use your VM's public IP (works because curl worked)
+vm_ip = "199.247.26.114"
+public_url = ngrok.connect(f"http://{vm_ip}:8000")
+print("Public HTTPS URL:", public_url)
+
+# Keep this script running while demoing
